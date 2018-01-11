6bd1a3132da89975ecdfdb6e0780012f695aec71
jc2744375 <juwanconaway@gmail.com>
2018-01-11 14:09:34 -0800

    Signed-off-by: jc2744375 <juwanconaway@gmail.com>

---
+++ Lab/New Folder/Menu/main.cpp
@@ -0,0 +1,45 @@
+/* 
+ * File:   main.cpp
+ * Author: Juwan Coanway
+ * Created on January 11, 2018, 1:17 PM
+ * Purpose: Validate the Truth Table
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    int probNumber;
+    
+    //Process
+    cout<<"Choose from the following menu"endl;
+    cout<<"Problem 1 -> Author_Edition__Chapter_Problem"<<endl;
+    cout<<"Problem 2 -> Author_Edition__Chapter_Problem"<<endl;
+    cout<<"Problem 3 -> Author_Edition__Chapter_Problem"<<endl;
+    cout<<"Problem 4 -> Author_Edition__Chapter_Problem"<<endl;
+    cout<<"Problem 5 -> Author_Edition__Chapter_Problem"<<endl;
+    cout<<"Problem 6 -> Author_Edition__Chapter_Problem"<<endl;
+    cout<<"Problem 7 -> Author_Edition__Chapter_Problem"<<endl;
+    cout<<"Problem 8 -> Author_Edition__Chapter_Problem"<<endl;
+    cout<<"Problem 9 -> Author_Edition__Chapter_Problem"<<endl;
+    cout<<"type 1 to 9 only"<<endl;
+    cin<<probNumber;
+    
+    //output the 
+    
+    
+
+    //Exit
+    return 0;
+}
+
---
+++ Lab/New Folder/TruthTable/main.cpp
@@ -0,0 +1,103 @@
+/* 
+ * File:   main.cpp
+ * Author: Juwan Coanway
+ * Created on January 11, 2018, 1:17 PM
+ * Purpose: Validate the Truth Table
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Only, No Global Variables
+//Like PI, e, Gravity, or conversions
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    bool x,y;
+    
+    //Generate the Heading of the Truth Table
+    cout<<"X Y !X !Y X&&Y X||Y X^Y X^Y^Y X^Y^X "
+        <<"!(X&&y) !X||!Y !(X||Y) !X&&!Y"<<endl;
+    
+    //First Row
+    x=true;
+    y=true;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<" ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"  ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x||y?'T':'F')<<"     ";
+    cout<<(x^y?'T':'F')<<"     ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"     ";
+    cout<<(!(x&&y)?'T':'F')<<"     ";
+    cout<<(!x||!y?'T':'F')<<"        ";
+    cout<<(!(x||y)?'T':'F')<<"         ";
+    cout<<(!x&&!y?'T':'F')<<"          ";
+    cout<<endl;
+    
+    //Second Row
+    x=true;
+    y=false;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<" ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"  ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x||y?'T':'F')<<"     ";
+    cout<<(x^y?'T':'F')<<"     ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"     ";
+    cout<<(!(x&&y)?'T':'F')<<"     ";
+    cout<<(!x||!y?'T':'F')<<"        ";
+    cout<<(!(x||y)?'T':'F')<<"         ";
+    cout<<(!x&&!y?'T':'F')<<"          ";
+    cout<<endl;
+    
+    //Third Row
+    x=false;
+    y=true;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<" ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"  ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x||y?'T':'F')<<"     ";
+    cout<<(x^y?'T':'F')<<"     ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"     ";
+    cout<<(!(x&&y)?'T':'F')<<"     ";
+    cout<<(!x||!y?'T':'F')<<"        ";
+    cout<<(!(x||y)?'T':'F')<<"         ";
+    cout<<(!x&&!y?'T':'F')<<"          ";
+    cout<<endl;
+    
+    //Fourth Row
+    x=false;
+    y=false;
+    cout<<(x?'T':'F')<<" ";
+    cout<<(y?'T':'F')<<" ";
+    cout<<(!x?'T':'F')<<"  ";
+    cout<<(!y?'T':'F')<<"  ";
+    cout<<(x&&y?'T':'F')<<"    ";
+    cout<<(x||y?'T':'F')<<"     ";
+    cout<<(x^y?'T':'F')<<"     ";
+    cout<<(x^y^y?'T':'F')<<"     ";
+    cout<<(x^y^x?'T':'F')<<"     ";
+    cout<<(!(x&&y)?'T':'F')<<"     ";
+    cout<<(!x||!y?'T':'F')<<"        ";
+    cout<<(!(x||y)?'T':'F')<<"         ";
+    cout<<(!x&&!y?'T':'F')<<"          ";
+    cout<<endl;
+
+    //Exit
+    return 0;
+}
+
